6bbf03ebde573bcf88ea799bdc98f545b5926ac2
Benaamar <benaamar-mossaab@outlook.com>
2026-01-06 16:49:32 +0100

    Add apostrophe separator between 3 digits in portfolio amount

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -228,7 +228,7 @@ function Dashboard() {
   </div>
 
   <div className="portfolio-main">
-    <span className="portfolio-amount">CHF {adminBalance.toLocaleString('fr-CH', { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</span>
+    <span className="portfolio-amount">CHF {adminBalance.toLocaleString('fr-CH').replace(/\B(?=(\d{3})+(?!\d))/g, "'")}</span>
     <div className="portfolio-gain">
       <span className="gain-percent">+2.04%</span>
       <span className="gain-amount">CHF 1'043</span>
